70e42d1d2cf7a7f84d5fc97e75089351c3827b2c
YMMYMarketing <santasuburbia@gmail.com>
2024-08-21 14:46:03 -0400

    Updates

---
+++ precache-manifest.267826ceef08bae8f929395f1ed22a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e743fa5566fc12681651703cdc5c45c8",
+    "url": "/lead-generation-dashboard/index.html"
+  },
+  {
+    "revision": "bb24102b8aea7c1337f0",
+    "url": "/lead-generation-dashboard/static/css/main.1526a4d4.chunk.css"
+  },
+  {
+    "revision": "004f945ae9e280cdb1b6",
+    "url": "/lead-generation-dashboard/static/js/2.f55a9306.chunk.js"
+  },
+  {
+    "revision": "51e794b51de55750b2ec",
+    "url": "/lead-generation-dashboard/static/js/3.45be8e5e.chunk.js"
+  },
+  {
+    "revision": "bb24102b8aea7c1337f0",
+    "url": "/lead-generation-dashboard/static/js/main.a80b5f7d.chunk.js"
+  },
+  {
+    "revision": "68adca9a75274fe1256b",
+    "url": "/lead-generation-dashboard/static/js/runtime~main.d8daf02d.js"
+  }
+]);
